54d48081761fe78236d1efe00f1c1f88386915ec
Moarqi <mrothgaenger@techfak.uni-bielefeld.de>
2019-06-19 01:26:46 +0200

    cannot belive my eyes, actually works

--- ServerThread.py
+++ ServerThread.py
@@ -13,7 +13,6 @@ class ServerThread(Thread):
         self.do_stop = Event()
         self.score = score
         self.running_servers = {}
-        self.waiting = []
         Thread.__init__(self)
 
     # overwrite join to set the event
@@ -29,6 +28,7 @@ class ServerThread(Thread):
                 continue
             try:
                 _socket = socket.create_connection(('localhost', port))
+                _socket.setblocking(0)
                 _socket.send(bytes(f'[INFO] {self.port} {self.score}\n', 'utf-8'))
                 print(f'notify sent to {port}')
                 self.inputs.append(_socket) # this line is critical!
@@ -38,17 +38,31 @@ class ServerThread(Thread):
 
 
     def process_recieved_data(self, data):
+        self.score += get_clock_info('process_time').resolution
         info, port, score = data.split(' ')
         self.running_servers[port] = score
+        max_score = float(max([server for server in self.running_servers.values()]))
+        print(self.score, max_score)
+        if self.score > max_score:
+            print('I am the boss :)')
+        else:
+            max_port = max(
+                self.running_servers,
+                key=lambda s: self.running_servers[s]
+            )
+            print(f"{max_port} is the boss :(")
         print(self.running_servers)
 
 
     def run(self):
         self.notify_running_servers()
+        respond_to = []
+
         while not self.do_stop.isSet():
             readable_sockets, writable_sockets, error_sockets = (
-                select(self.inputs, self.outputs, self.waiting, 0.5)
+                select(self.inputs, self.outputs, self.inputs, 1)
             )
+
             for _socket in readable_sockets:
                 # the server itself is the one that can accept new connections
                 if _socket is self.server_socket:
@@ -56,36 +70,63 @@ class ServerThread(Thread):
                     receive_socket, addr = _socket.accept()
                     receive_socket.setblocking(0)
                     self.inputs.append(receive_socket)
+                    self.outputs.extend(respond_to)
+
+                    if receive_socket not in self.outputs:
+                        self.outputs.append(receive_socket)
 
                 else:
                     recievedbytes = _socket.recv(1024)
+
                     if recievedbytes:
                         recieved_data = recievedbytes.decode('utf-8')
                         print(recieved_data)
+                        # problem is, every time the other server sends an info, this server
+                        # responds with an info -> inf loop!
                         if '[INFO]' in recieved_data:
                             self.process_recieved_data(recieved_data)
-                        if _socket not in self.outputs:
-                            self.outputs.append(_socket)
+                        if _socket not in respond_to:
+                            respond_to.append(_socket)
+
                     else:
                         self.inputs.remove(_socket)
                         print('closing')
+
                         if _socket in self.outputs:
                             self.outputs.remove(_socket)
+                        if _socket in respond_to:
+                            respond_to.remove(_socket)
+
+                        self.running_servers.clear()
+                        self.outputs.extend(respond_to)
+
                         _socket.close()
 
             for _socket in writable_sockets:
-                self.score += get_clock_info('process_time').resolution
-                _socket.send(bytes(f'[INFO] {self.port} {self.score}\n', 'utf-8'))
-                self.outputs.remove(_socket)
-                self.inputs.remove(_socket)
-                self.waiting.append(_socket)
+                if _socket is not self.server_socket and _socket in respond_to:
+                    self.score += get_clock_info('process_time').resolution
+                    _socket.send(bytes(f'[INFO] {self.port} {self.score}\n', 'utf-8'))
+                    # respond_to.remove(_socket)
+                    self.outputs.remove(_socket)
 
             for _socket in error_sockets:
-                print('closing')
+                print('closing bc of error')
                 self.inputs.remove(_socket)
+
                 if _socket in self.outputs:
                     self.outputs.remove(_socket)
+
                 _socket.close()
+
+        ### SHUTDOWN ###
+        for _socket in self.inputs:
+            _socket.close()
+
+        for _socket in self.outputs:
+            _socket.close()
+
+        for _socket in respond_to:
+            _socket.close()
 
 
 
